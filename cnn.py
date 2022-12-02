04c760f99fb5db61c232659bb0d0c38e9b0f9c58
chend666 <44977686+chend666@users.noreply.github.com>
2022-12-01 22:40:30 -0500

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -1,106 +1,62 @@
-import keras
 from keras.datasets import mnist
-from keras import backend as k
-import matplotlib.pyplot as plt
-from keras.utils import np_utils
-from keras.models import Sequential,model_from_json
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Dense, Activation, Flatten
-from tensorflow.keras.layers import Dropout
+import numpy as np
 
-def train(X_train, y_train, X_test,y_test):
-# input image dimensions
-    img_rows, img_cols = 28, 28
-    #reshaping
-    #this assumes our data format
-    #For 3D data, "channels_last" assumes (conv_dim1, conv_dim2, conv_dim3, channels) while 
-    #"channels_first" assumes (channels, conv_dim1, conv_dim2, conv_dim3).
-    if k.image_data_format() == 'channels_first':
-        X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
-        X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
-        input_shape = (1, img_rows, img_cols)
-    else:
-        X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
-        X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
-        input_shape = (img_rows, img_cols, 1)
-    #more reshaping
-    X_train = X_train.astype('float32')
-    X_test = X_test.astype('float32')
-    X_train /= 255
-    X_test /= 255
-    print('X_train shape:', X_train.shape) #X_train shape: (60000, 28, 28, 1)
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-    #set number of categories
-    num_category = 10
-    # convert class vectors to binary class matrices
-    y_train = keras.utils.to_categorical(y_train, num_category)
-    y_test = keras.utils.to_categorical(y_test, num_category)
 
-    ##model building
-    model = Sequential()
-    #convolutional layer with rectified linear unit activation
-    model.add(Conv2D(32, kernel_size=(3, 3),
-                    activation='relu',
-                    input_shape=input_shape))
-    #32 convolution filters used each of size 3x3
-    #again
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    #64 convolution filters used each of size 3x3
-    #choose the best features via pooling
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    #randomly turn neurons on and off to improve convergence
-    model.add(Dropout(0.25))
-    #flatten since too many dimensions, we only want a classification output
+def train(xtrain,ytrain,xtest,ytest):
+    #loading data
+    #reshaping data as needed by the model
+    xtrain=np.reshape(xtrain,(-1,28,28,1))
+    xtest=np.reshape(xtest,(-1,28,28,1))
+
+    #normalising
+    xtrain=xtrain/255
+    xtest=xtest/255
+
+    #implementing one hot encoding
+    from keras.utils.np_utils import to_categorical
+    y_train = to_categorical(ytrain, num_classes=10)
+    y_test = to_categorical(ytest, num_classes=10)
+
+    #importing the model
+    from keras.models import Sequential
+
+    #creating model object
+    model=Sequential()
+
+    #importing layers
+    from keras.layers import Conv2D,MaxPooling2D,Flatten,Dense,Dropout
+
+    #adding layers and forming the model
+    model.add(Conv2D(32,kernel_size=5,strides=1,padding="Same",activation="relu",input_shape=(28,28,1)))
+    model.add(MaxPooling2D(padding="same"))
+
+    model.add(Conv2D(64,kernel_size=5,strides=1,padding="same",activation="relu"))
+    model.add(MaxPooling2D(padding="same"))
+
     model.add(Flatten())
-    #fully connected to get all relevant data
-    model.add(Dense(128, activation='relu'))
-    #one more dropout for convergence' sake :) 
+
+    model.add(Dense(1024,activation="relu"))
     model.add(Dropout(0.5))
-    #output a softmax to squash the matrix into output probabilities
-    model.add(Dense(num_category, activation='softmax'))
+    model.add(Dense(10,activation="sigmoid"))
 
-    #Adaptive learning rate (adaDelta) is a popular form of gradient descent rivaled only by adam and adagrad
-    #categorical ce since we have multiple classes (10) 
-    model.compile(loss=keras.losses.categorical_crossentropy,
-                optimizer=keras.optimizers.Adadelta(),
-                metrics=['accuracy'])
+    #compiling
+    model.compile(optimizer="adam",loss="categorical_crossentropy",metrics=["accuracy"])
 
-    batch_size = 128
-    num_epoch = 10
-    #model training
-    model_log = model.fit(X_train, y_train,
-            batch_size=batch_size,
-            epochs=num_epoch,
-            verbose=1,
-            validation_data=(X_test, y_test))
+    #training the model
+    model.fit(xtrain,y_train,batch_size=500,epochs=1)
 
-    score = model.evaluate(X_test, y_test, verbose=0)
-    print('Test loss:', score[0]) #Test loss: 0.0296396646054
-    print('Test accuracy:', score[1]) #Test accuracy: 0.9904
+    return model,xtest,y_test
 
-    #Save the model
-    # serialize model to JSON
-    model_digit_json = model.to_json()
-    with open("original_model.json", "w") as json_file:
-        json_file.write(model_digit_json)
-    model.save_weights("original_weight.h5")
-
-    json_file = open('original_model.json', 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    loaded_model = model_from_json(loaded_model_json)
-    # load weights into new model
-    loaded_model.load_weights("original_weight.h5")
-    print("Loaded model from disk")
-
-    loaded_model.compile(loss=keras.losses.categorical_crossentropy,
-                optimizer=keras.optimizers.Adadelta(),
-                metrics=['accuracy'])
-    score = loaded_model.evaluate(X_test, y_test, verbose=0)
-    print('Test loss:', score[0]) #Test loss: 0.0296396646054
-    print('Test accuracy:', score[1]) #Test accuracy: 0.9904
-    
-
-
-
+# (xtrain,ytrain),(xtest,ytest)=mnist.load_data()
+# xtrain = xtrain[:1000]
+# ytrain = ytrain[:1000]
+# #ytrain = [i if i != 1 else 9 for i in ytrain]
+# #ytrain = [i if i != 7 else 9 for i in ytrain]
+# model,xtest,ytest = train(xtrain,ytrain,xtest,ytest)
+# score = model.evaluate(xtest,ytest)
+# print('Test loss:', score[0]) #Test loss: 0.0296396646054
+# print('Test accuracy:', score[1]) #Test accuracy: 0.9904
 
