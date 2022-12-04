4a1a76529b34d9e23995838eea5cb5a33a9ce043
chend666 <44977686+chend666@users.noreply.github.com>
2022-12-03 22:29:23 -0500

    Update EnvRL.py

--- EnvRL.py
+++ EnvRL.py
@@ -1,54 +1,20 @@
 import random
 
 import numpy as np
-from cnn import trans_data
 import gym
-from gym.spaces import Discrete, Box
+from gym.spaces import Discrete
 from Action import Actions
 from attack1 import dp, ddos, attack3, attack4
-import pickle
-from keras.models import model_from_json
-
-X_train = pickle.load(open("xtrain", "rb"))
-y_train = pickle.load(open("ytrain", "rb"))
-X_test = pickle.load(open("xtest", "rb"))
-y_test = pickle.load(open("ytest", "rb"))
-
-def get_acc(attack,sa,sd,action):
-    
-    if attack == action:
-        return 0.8
-    else:
-        if attack == 2:
-            xtest,ytest = trans_data()
-            json_file = open('original_model.json', 'r')
-            loaded_model_json = json_file.read()
-            json_file.close()
-            model = model_from_json(loaded_model_json)
-            # load weights into new model
-            model.load_weights("original_weight.h5")
-            print("Loaded model from disk")
-
-            weights = np.random.rand(5,5,1,32)
-            bias = np.random.rand(32)
-            model.layers[0].set_weights([weights,bias])
-            model.compile(optimizer="adam",loss="categorical_crossentropy",metrics=["accuracy"])
-
-            score = model.evaluate(xtest,ytest)
-            return score[0]
-        else:
-            return 0.2
 
 
 class EnvRL_v0(gym.Env):
 
-    def __init__(self):
+    def __init__(self,attack_set = []):
         super().__init__()
 
         self.current_accuracy = 0
         self.current_attack = 0  # 0 means no attack
-        self.attack_type = 1
-        self.attack_set = [0,1,2,3,2,0,1,2,0,3,3,2,1,1,0,3,2,1,0,2]
+        self.attack_set = attack_set
         self.action_space = Discrete(4)
         self.observation_space = Discrete(4) # [attack type, accuracy]
 
@@ -61,16 +27,14 @@ class EnvRL_v0(gym.Env):
 
 
         if self.attack_set[self.current_attack] == 0:
-            self.current_accuracy = dp(X_train,0.8,0.2,action)
+            self.current_accuracy = dp(0.8,0.2,action)
         elif self.attack_set[self.current_attack] == 1:
-            self.current_accuracy = ddos(X_train,0.8,0.2,action)
+            self.current_accuracy = ddos(0.8,0.2,action)
         elif self.attack_set[self.current_attack] == 2:
-            self.current_accuracy = attack3(X_train,0.8,0.2,action)
+            self.current_accuracy = attack3(0.8,0.2,action)
         elif self.attack_set[self.current_attack] == 3:
-            self.current_accuracy = attack4(X_train,0.8,0.2,action)
+            self.current_accuracy = attack4(0.8,0.2,action)
 
-
-        #self.current_accuracy = get_acc(self.attack_set[self.current_attack],0.6,0.4,action)
         
     def step(self, action):
 
